1bd4f78a4d1f14a96714500d9d6a09b516a8b695
Alex <schwimmeralexander@gmail.com>
2024-06-07 11:10:28 -0700

    Create a new project in src and use addTodo method to push it to project list array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { createTodo } from "./components/todo-constructor.js";
-
+import { createProject } from "./components/project-constructor.js";
 const newTodo = createTodo("homework", "do it", "now", 0);
+const newProject = createProject("default");
 
-console.log(newTodo);
+console.log(newProject);
+newProject.addTodo(newTodo);
+console.log(newProject);
+
