9dcd6d605e4172f8021a50a54087c369983d79d9
Alex <schwimmeralexander@gmail.com>
2024-06-08 13:26:24 -0700

    Add variables for testing changes to todo constructer function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { createTodo } from "./components/todo-constructor.js";
 import { createProject } from "./components/project-constructor.js";
-const newTodo = createTodo("homework", "do it", "now", 0);
-const newProject = createProject("default");
+import { setPriority } from "./components/set-priority.js";
+import { setCompleteStatus } from "./components/set-complete.js";
 
+const homework = createTodo("homework", "do it", "now", 0);
+const walkDog = createTodo("walkies", "walk dog", "evening", 1);
+const feedCat = createTodo("feed cat", "noon", 2);
+
+const newProject = createProject("default");
+newProject.addTodo(homework);
+newProject.addTodo(walkDog);
+newProject.addTodo(feedCat);
 console.log(newProject);
-newProject.addTodo(newTodo);
 console.log(newProject);
 
