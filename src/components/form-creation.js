7465438961e356bd5e3468f44e2d58bddfc565e3
Alex <schwimmeralexander@gmail.com>
2024-06-10 13:15:51 -0700

    Complete component that create a form with labels and appends the form to the HTML template

--- src/components/form-creation.js
+++ src/components/form-creation.js
@@ -1,3 +1,31 @@
 function createForm() {
     const form = document.createElement("form");
+    
+    const createInput = (type, name, id, label) => {
+        const input = document.createElement("input");
+        input.type = type;
+        input.name = name;
+        input.id = id;
+        const inputLabel = document.createElement("label");
+        inputLabel.for = id;
+        inputLabel.innerText = label;
+        return { input, inputLabel };
+    };
+
+    const { input: priority, inputLabel: priorityLabel } = createInput("number", "priority", "priority", "Priority");
+    const { input: title, inputLabel: titleLabel } = createInput("text", "title", "title", "Title");
+    const { input: description, inputLabel: descriptionLabel } = createInput("text", "description", "description", "Description");
+
+    form.append(
+        priorityLabel,
+        priority,
+        titleLabel,
+        title,
+        descriptionLabel,
+        description
+    );
+
+    return form;
 }
+
+export { createForm }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createTodo } from "./components/todo-constructor.js";
 import { createProject } from "./components/project-constructor.js";
 import { setPriority } from "./components/set-priority.js";
 import { setCompleteStatus } from "./components/set-complete.js";
-
+import { createForm } from "./components/form-creation.js";
 const homework = createTodo("homework", "do it", "now", 0);
 const walkDog = createTodo("walkies", "walk dog", "evening", 1);
 const feedCat = createTodo("feed cat", "noon", 2);
@@ -13,4 +13,8 @@ newProject.addTodo(walkDog);
 newProject.addTodo(feedCat);
 console.log(newProject);
 console.log(newProject);
+
+const content = document.getElementById("content");
+const form = createForm();
+content.appendChild(form);
 
