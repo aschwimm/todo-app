f87b0de156aae68d0d1a801e0acda9735034bf70
Alex <schwimmeralexander@gmail.com>
2024-06-20 16:46:41 -0700

    Create component that generates a todo form

---
+++ src/components/todo-form.js
@@ -0,0 +1,30 @@
+function todoForm() {
+    const form = document.createElement("form");
+    const capitalizeLabel = label => label.charAt(0).toUpperCase() + label.slice(1);
+    const formConstructor = (name, type) => {
+        const input = document.createElement("input");
+        const label = document.createElement("label");
+        input.name = name;
+        input.type = type;
+        label.for = name;
+        label.innerHTML = capitalizeLabel(name);
+        return {input, label};
+    }
+    const {input: priority, label: priorityLabel } = formConstructor("priority", "number");
+    const {input: title, label: titleLabel } = formConstructor("title", "text");
+    const {input: description, label: descriptionLabel} = formConstructor("description", "text");
+    const {input: date, label: dateLabel} = formConstructor("date", "datetime-local");
+    form.append(
+        priorityLabel,
+        priority,
+        titleLabel,
+        title,
+        descriptionLabel,
+        description,
+        dateLabel,
+        date
+    )
+    return form;
+}
+
+export { todoForm }
