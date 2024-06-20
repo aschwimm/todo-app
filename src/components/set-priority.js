7ffdcfb0fa71f01f65605c97326ead9c376343fd
Alex <schwimmeralexander@gmail.com>
2024-06-20 16:20:17 -0700

    Remove all components and index.js for clean start

--- src/components/set-priority.js
+++
@@ -1,7 +0,0 @@
-function setPriority(priority, todo) {
-    const priorityLevel = ["high", "medium", "low"];
-    todo.priority = priorityLevel[priority];
-    return todo;
-}
-
-export { setPriority }
